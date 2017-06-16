1d66ecd3d165631417c1a0dd10201e60628c9439
vikasdesale <vikas.desaleu@gmail.com>
2017-06-16 11:49:57 +0530

    FavoriteToys:1.2_Display_toys

--- FavoriteToys/app/src/main/java/com/android/developers/favoritetoys/MainActivity.java
+++ FavoriteToys/app/src/main/java/com/android/developers/favoritetoys/MainActivity.java
@@ -2,12 +2,19 @@ package com.android.developers.favoritetoys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView mToysListTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToysListTextView = (TextView) findViewById(R.id.tv_toy_names);
+        String[] toyNames = ToyBox.getToyNames();
+        for (String toyName : toyNames) {
+            mToysListTextView.append(toyName + "\n\n\n");
+        }
     }
 }
